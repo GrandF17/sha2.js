25bc5bc65af66f7da6ae443143be128f01b30467
GrandF17 <bebe>
2026-01-17 15:33:10 +0300

    clean up

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
 /** SHA224/256 */
 
 export { createHash } from "@/utils/helpers";
-
-import { createHash } from '@/utils/helpers';
-
-const paylaod = new Uint8Array(104_857_600);
-const a = createHash("sha256").update(paylaod).digest();
-console.log(Buffer.from(a).toString("hex"));
