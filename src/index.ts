037c1ac34f894b91fffdbe0438a93eeab0a0186e
GrandF17 <bebe>
2026-01-17 15:04:48 +0300

    got rid of sha284/512 because of complexity of implementation for JS number type (only 55 bits are allocated for uint part, sha384/512 needs u64

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,3 @@
-/** 
- * SHA224/256/384/512 
- */
+/** SHA224/256 */
 
-import { sha256 } from '@noble/hashes/sha2.js';
-import { SHA224, SHA256 } from "@/classes";
-import { createHash } from '@/utils/helpers';
-// import { createHash } from "crypto";
-
-
-const paylaod = new Uint8Array(104_857_600);
-const a = createHash("sha256").update(paylaod).digest();
-console.log(Buffer.from(a).toString("hex"));
-
-// const b = sha256.create().update(paylaod).digest();
-// console.log(Buffer.from(b).toString("hex"));
-
-// const c = new SHA224().update(paylaod).digest();
-// console.log(Buffer.from(c).toString("hex"));
-
-// const d = createHash("sha256").update(paylaod).digest("hex");
-// console.log(d);
+export { createHash } from "@/utils/helpers";
