3e419a58dc1a82c01863a2a1e8b3ca62c27c8c4b
GrandF17 <v.o.nikolenko@gmail.com>
2026-01-19 12:10:35 +0300

    removed all test code from index and added readme hmac-sha256 example

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,3 @@
-/** SHA224/256 */
+/** SHA224/256 + HMAC */
 
-import { createHmac } from "@/utils/helpers";
-import { SHA256 } from "./classes";
-import { HMAC } from "./classes/hmac";
-
-export { createHash } from "@/utils/helpers";
-
-const key = Buffer.from("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b", "hex");
-const data = Buffer.from("4869205468657265", "hex");
-
-const hmac = createHmac("sha256").init(key);
-
-console.log(Buffer.from(hmac.update(data).digest()).toString("hex"))
+export { createHash, createHmac } from "@/utils/helpers";
