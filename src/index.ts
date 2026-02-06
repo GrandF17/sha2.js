c2752ca49f26acf6a376a8ec9f9e9f63d25356ba
GrandF17 <v.o.nikolenko@gmail.com>
2026-02-06 14:21:15 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,3 @@
 /** SHA224/256 + HMAC */
 
-import { createHash } from "@/utils/helpers";
-
 export { createHash, createHmac } from "@/utils/helpers";
-
-const sha = createHash("sha256");
-
-const paylaod = new Uint8Array(104_857_600);
-
-/** creating hash from payload */
-const result = sha.update(paylaod).digest();
-
-/** logging the result of hash-function */
-console.log("sha256():", Buffer.from(result).toString("hex"));
